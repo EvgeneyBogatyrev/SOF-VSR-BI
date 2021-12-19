b1cd257ec0a5a387068072a01db1fee6a5a2f1e2
Evgeney Zimin <evgeneyz@vg-gpu-01.lab.graphicon.ru>
2021-12-19 14:15:44 +0300

    Fix

--- run.py
+++ run.py
@@ -3,12 +3,12 @@ import os
 with open("run.sh", 'w') as f:
     _, vids, _ = next(os.walk("/dataset"))
 
-    f.write("mkdir results")
+    f.write("mkdir results\n")
 
     for vid in vids:
-        f.write("python3 TIP/demo_my.py --testset_dir /dataset --video_name {vid} --save_result_dir results --degradation BI")
+        f.write("python3 TIP/demo_my.py --testset_dir /dataset --video_name {vid} --save_result_dir results --degradation BI\n")
 
-    f.write("chmod -R 0777 /model")
+    f.write("chmod -R 0777 /model\n")
 
 os.system("chmod +x run.sh")
 os.system("./run.sh")
