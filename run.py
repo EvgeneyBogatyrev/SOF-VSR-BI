0e4e12ae9cf523dcbc236c6caa1a519da645ab27
Evgeney Zimin <evgeneyz@vg-gpu-01.lab.graphicon.ru>
2022-01-04 20:25:55 +0300

    Change interface

--- run.py
+++ run.py
@@ -3,12 +3,10 @@ import os
 with open("/model/run.sh", 'w') as f:
     _, vids, _ = next(os.walk("/dataset"))
 
-    f.write("mkdir /model/result\n")
-
     for vid in vids:
-        f.write(f"python3 /model/TIP/demo_my.py --testset_dir /dataset --video_name {vid} --save_result_dir /model/result --degradation BI\n")
+        f.write(f"python3 /model/TIP/demo_my.py --testset_dir /dataset --video_name {vid} --save_result_dir /results --degradation BI\n")
 
-    f.write("chmod -R 0777 /model\n")
+    f.write("chmod -R 0777 /results\n")
 
 os.system("chmod 0777 /model/run.sh")
 os.system("/model/run.sh")
--- run.sh
+++ run.sh
@@ -1,4 +1,2 @@
-mkdir /model/result
-python3 /model/TIP/demo_my.py --testset_dir /dataset --video_name gauss --save_result_dir /model/result --degradation BI
-python3 /model/TIP/demo_my.py --testset_dir /dataset --video_name bicubic --save_result_dir /model/result --degradation BI
-chmod -R 0777 /model
+python3 /model/TIP/demo_my.py --testset_dir /dataset --video_name bicubic --save_result_dir /results --degradation BI
+chmod -R 0777 /results
